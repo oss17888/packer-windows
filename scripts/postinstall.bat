9491ce6b1058f27881c646b5f847d4a861b311dd
rick <oss17888@gmail.com>
2016-11-28 13:42:27 +0800

    update

---
+++ scripts/postinstall.bat
@@ -0,0 +1,3 @@
+cmd /c "C:\Python27\python.exe" -m pip install --upgrade pip
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-ride
