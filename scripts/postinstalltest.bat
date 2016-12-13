3f726890429a2930467ec78c85d77cdab1367e7d
Rick Chang <rickc@aten.com.tw>
2016-12-13 11:53:18 +0800

    update for test

--- scripts/postinstalltest.bat
+++ scripts/postinstalltest.bat
@@ -1,7 +1,2 @@
-cmd /c "C:\Python27\python.exe" -m pip install --upgrade pip
-cmd /c "C:\Python27\Scripts\pip.exe" install robotframework
-cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-ride
-cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-selenium2library
-
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://effbot.org/downloads/PIL-1.1.7.win32-py2.7.exe')" <NUL
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/robotframework-autoitlibrary/AutoItLibrary-1.1.zip')" <NUL
