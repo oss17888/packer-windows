94d9abaf6b3107c6aaa4751c0b460a9fe4edd614
rick <oss17888@gmail.com>
2016-12-13 12:03:22 +0800

    merge chocolate.ps1 postinstall.bat

--- scripts/postinstall.bat
+++ scripts/postinstall.bat
@@ -1,3 +1,19 @@
 cmd /c "C:\Python27\python.exe" -m pip install --upgrade pip
 cmd /c "C:\Python27\Scripts\pip.exe" install robotframework
 cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-ride
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-selenium2library
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-extendedselenium2library
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-appiumlibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install -U robotframework-difflibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-archivelibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install pillow==2.9.0
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-imagehorizonlibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install --upgrade robotframework-ioslibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install --upgrade robotframework-androidlibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-databaselibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-faker
+cmd /c "C:\Python27\Scripts\pip.exe" install --upgrade robotframework-httplibrary
+cmd /c "C:\Python27\Scripts\pip.exe" install -U requests
+cmd /c "C:\Python27\Scripts\pip.exe" install -U robotframework-requests
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-rammbock
+cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-sshlibrary
