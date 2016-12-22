71761d01d6b5c8215e5d0f3aa5b23bfc77a90130
Rick Chang <rickc@aten.com.tw>
2016-12-22 10:05:36 +0800

    update

--- scripts/postinstall.bat
+++ scripts/postinstall.bat
@@ -16,9 +16,9 @@ cmd /c "C:\Python27\Scripts\pip.exe" install --upgrade robotframework-httplibrar
 cmd /c "C:\Python27\Scripts\pip.exe" install -U requests
 cmd /c "C:\Python27\Scripts\pip.exe" install -U robotframework-requests
 cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-rammbock
-cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-sshlibrary
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lightkeeper/lswindows-lib/blob/master/amd64/python/PIL-1.1.7.win-amd64-py2.7.exe?raw=true', 'C:\Users\vagrant\PIL117.exe')" <NUL
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/robotframework-autoitlibrary/AutoItLibrary-1.1.zip', 'C:\Users\vagrant\AutoItLibrary-1.1.zip')" <NUL
+# cmd /c "C:\Python27\Scripts\pip.exe" install robotframework-sshlibrary
+# powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lightkeeper/lswindows-lib/blob/master/amd64/python/PIL-1.1.7.win-amd64-py2.7.exe?raw=true', 'C:\Users\vagrant\PIL117.exe')" <NUL
+# powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/robotframework-autoitlibrary/AutoItLibrary-1.1.zip', 'C:\Users\vagrant\AutoItLibrary-1.1.zip')" <NUL
 # cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Users\vagrant\AutoItLibrary-1.1.zip -oC:\Users\vagrant"
 # cd AutoItLibrary-1.1
 # cmd /c "C:\Python27\python.exe" setup.py install
